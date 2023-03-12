02ada5b0e7754a4328a66ccc7d01217ab0b54d41
godzzz <g0dzZz1lla@yandex.ru>
2023-03-13 02:13:30 +0400

    Xml documentation

--- Singletons/Entities/LazySingleton.cs
+++ Singletons/Entities/LazySingleton.cs
@@ -3,13 +3,27 @@ using Depra.Singletons.Attributes;
 
 namespace Depra.Singletons.Entities
 {
+    /// <summary>
+    /// Thread-safe implementation of the Singleton pattern with lazy initialization of a generic type <see cref="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of object that the singleton should create.</typeparam>
+    /// <remarks>
+    /// This class guarantees that only one instance of type <see cref="T"/>
+    /// is created during the lifetime of the application.
+    /// </remarks>
     [Singleton]
     public class LazySingleton<T> where T : class, new()
     {
         private static readonly Lazy<T> LAZY = new Lazy<T>(() => new T());
 
+        /// <summary>
+        /// Gets the singleton instance of the specified generic type <see cref="T"/>.
+        /// </summary>
         public static T Instance => LAZY.Value;
 
+        /// <summary>
+        /// Protected constructor to prevent instantiation of this class from outside.
+        /// </summary>
         protected LazySingleton() { }
     }
 }
--- Singletons/Entities/Singleton.cs
+++ Singletons/Entities/Singleton.cs
@@ -3,12 +3,23 @@ using Depra.Singletons.Attributes;
 
 namespace Depra.Singletons.Entities
 {
+    /// <summary>
+    /// Thread-safe implementation of the Singleton pattern with eager initialization of a generic type <see cref="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of object that the singleton should create.</typeparam>
+    /// <remarks>
+    /// This class guarantees that only one instance of type <see cref="T"/>
+    /// is created during the lifetime of the application.
+    /// </remarks>
     [Singleton]
     public class Singleton<T> where T : class, new()
     {
         private static readonly object OBJECT_LOCK = new object();
         private static T _instance;
 
+        /// <summary>
+        /// Gets the singleton instance of the specified generic type <see cref="T"/>.
+        /// </summary>
         public static T Instance
         {
             get
@@ -30,5 +41,10 @@ namespace Depra.Singletons.Entities
                 _instance = new T();
             }
         }
+
+        /// <summary>
+        /// Protected constructor to prevent instantiation of this class from outside.
+        /// </summary>
+        protected Singleton() { }
     }
 }
